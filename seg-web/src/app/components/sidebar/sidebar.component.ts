29650dfb9bc63f227b0055eed397df37a74ae8fb
Mouhamadou Moustapha Dia <diatapha00@gmail.com>
2022-08-20 02:50:18 +0200

    Mise en place Front et les forms

--- seg-web/src/app/components/sidebar/sidebar.component.ts
+++ seg-web/src/app/components/sidebar/sidebar.component.ts
@@ -11,11 +11,12 @@ export const ROUTES: RouteInfo[] = [
     { path: '/dashboard', title: 'Dashboard',  icon: 'dashboard', class: '' },
     { path: '/user-profile', title: 'Mon Profil',  icon:'person', class: '' },
     { path: '/table-list', title: 'Evénements',  icon:'content_paste', class: '' },
+    { path: '/maps', title: 'Signalez une zone',  icon: 'location_on', class: '' },
     { path: '/typography', title: 'Modérer un événement',  icon:'library_books', class: '' },
-    { path: '/icons', title: 'Icons',  icon:'bubble_chart', class: '' },
-    { path: '/maps', title: 'Maps',  icon:'location_on', class: '' },
+   // { path: '/icons', title: 'Icons',  icon:'bubble_chart', class: '' },
+   // { path: '/maps', title: 'Maps',  icon:'location_on', class: '' },
     { path: '/notifications', title: 'Notifications',  icon:'notifications', class: '' },
-    { path: '/upgrade', title: 'Upgrade to PRO',  icon:'unarchive', class: 'active-pro' },
+   // { path: '/upgrade', title: 'Upgrade to PRO',  icon:'unarchive', class: 'active-pro' },
 ];
 
 @Component({
